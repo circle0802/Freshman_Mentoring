300de2fb6b3b3151ac7f3485b032969b6896f8b7
원이 <hawon9781@gmail.com>
2026-03-06 10:52:11 +0900

    프로젝트 생성

---
+++ Memo/Memo/MemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoApp.swift
+//  Memo
+//
+//  Created by hawon on 3/6/26.
+//
+
+import SwiftUI
+
+@main
+struct MemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
